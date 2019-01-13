eae9f20c31549449e02460eb33877e0479d9094d
Drew Anderson <andrew@andersontech.org>
2019-01-12 20:44:18 -0500

    Define happiness

---
+++ happiness.py
@@ -0,0 +1,9 @@
+# Requires the web browser module to open the image
+import webbrowser
+
+# Happiness...defined 
+happiness = "yiss"
+print("Aww " + happiness + "...")
+
+# Opens CHEEZburger image
+webbrowser.open_new("https://i.chzbgr.com/full/7856613376/hCC9312B2/")
