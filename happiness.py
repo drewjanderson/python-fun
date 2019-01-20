e44f4234299e65a2810aea09ba6c72daf2cbd931
unknown <andrew@andersontech.org>
2019-01-19 20:49:29 -0500

    Happiness...after some sleep

--- happiness.py
+++ happiness.py
@@ -1,9 +1,11 @@
-# Requires the web browser module to open the image
+# Requires the web browser module to open the image and time...
 import webbrowser
+import time
 
 # Happiness...defined 
 happiness = "yiss"
 print("Aww " + happiness + "...")
+time.sleep(3)
 
 # Opens CHEEZburger image
 webbrowser.open_new("https://i.chzbgr.com/full/7856613376/hCC9312B2/")
