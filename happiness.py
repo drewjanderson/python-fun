895ddf9da23f201bcaf8a7619e5f3df65d9c0509
unknown <andrew@andersontech.org>
2019-01-19 21:14:16 -0500

    More fun experimenting while learning Python

--- happiness.py
+++ happiness.py
@@ -1,11 +1,31 @@
 # Requires the web browser module to open the image and time...
 import webbrowser
 import time
+import sys
+
+# Define helper function to print the following print on the same line
+def print_no_newline(string):
+    sys.stdout.write(string)
+    sys.stdout.flush()
+
+# Wait for it
+time.sleep(0.5)
+print("   Wait for it")
+print("   ")
+time.sleep(1)
 
 # Happiness...defined 
 happiness = "yiss"
-print("Aww " + happiness + "...")
-time.sleep(3)
+print_no_newline("   Aww ")
+time.sleep(0.5)
+print_no_newline(happiness)
+time.sleep(1)
+print_no_newline(".")
+time.sleep(1)
+print_no_newline(".")
+time.sleep(1)
+print_no_newline(".")
+time.sleep(1)
 
 # Opens CHEEZburger image
 webbrowser.open_new("https://i.chzbgr.com/full/7856613376/hCC9312B2/")
